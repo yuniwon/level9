51749107ad8112480af488fb1209c87bddd1b9be
yuniwon <dnjsdndus@gmail.com>
2023-12-14 16:41:52 +0900

    Create _04super생성자.java

---
+++ level9/src/상속_개념/_04super생성자.java
@@ -0,0 +1,29 @@
+package 상속_개념;
+
+class Product{
+	String name;
+	int price;
+	public Product(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+}
+class Fruit extends Product{
+	String origin;
+	public Fruit(String name, int price, String origin) {
+		super(name, price);
+		this.origin = origin;
+	}
+}
+public class _04super생성자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fruit f = new Fruit("사과", 1000, "대구");
+		System.out.println(f.name);
+		System.out.println(f.price);
+		System.out.println(f.origin);
+	}
+
+}
